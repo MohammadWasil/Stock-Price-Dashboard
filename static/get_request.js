6143f9806131a125e9153502337a8ad461e6af1f
MohammadWasil <wasilmohd1@gmail.com>
2021-10-28 01:50:58 +0200

    server setup and plotting

--- static/get_request.js
+++ static/get_request.js
@@ -1,40 +1,70 @@
-//function getData(){
-//    var xhttp = new XMLHttpRequest();
-//    xhttp.onreadystatechange = function(){
-//        if(this.readyState==4 && this.status==200){
-//          document.getElementById("database").innerHTML = this.responseText;
-//           console.log("WASIL")
-//        }
-//    };
-//    xhttp.open("GET", "get_data.py", true);
-//    xhttp.send();
-//    //console.log(strr)
-//}
+function processData(data){
+    //console.log(data['2021-10-25 09:25:00']['close']);
+    var close = new Array();
+    var open = new Array();
+    var high = new Array();
+    var low = new Array();
+    var timeStamp = new Array();
+
+    var time = new Array();
+    var date = new Array();
+
+    for (var x in data) {
+        close.push(data[x]['close']);
+        open.push(data[x]['open']);
+        high.push(data[x]['high']);
+        low.push(data[x]['low']);
+        timeStamp.push(String(x));
+     }
+
+     // split timeStampe between date and time.
+     for(var ts in timeStamp){
+        date.push(timeStamp[ts].split(" ")[0]);
+        time.push(timeStamp[ts].split(" ")[1]);
+     }
+
+     return {
+         close:close, 
+         open:open, 
+         high:high, 
+         low:low,
+         date:date,
+         time:time
+        };
+}
 
 var server = "http://127.0.0.1:5000";
 $(function() {
-        $("#IBMData").click(function() {
-        
-            var appdir = "/retrieveData"
-            var send_msg = "Clicked";
-            console.log(send_msg);
-            $('#message_1').html(send_msg);
+$("#IBMData").click(function() {
 
-            $.ajax({
-                type : "POST",
-                url : server+appdir
-                }).done(function(data) {
-                    console.log(data);
-                    $("#database").val(data);
-            });
+    var appdir = "/retrieveData"
+    var send_msg = "Clicked";
+    console.log(send_msg);
+    $('#message_1').html(send_msg);
 
-            //$.getJSON({
-            //    url: "/retrieveData",
-            //    data: { entry2_id: val, entry1_id: entry1 },
-            //    success: function(data){
-            //        $("#div1").html(data);
-            //    }
-            //});
-        });
+    $.ajax({
+        type : "POST",
+        url : server+appdir
+        }).done(function(data) {
+            console.log(data);
+
+            var values = processData(data);
+            var close = values.close;
+            var open = values.open;
+            var high = values.high;
+            var low = values.low;
+            var timeStamp = values.timeStamp;
+            var date = values.date;
+            var time = values.time;
+
+            // This will call in the function plotStocks to plot the stock price.
+            plotStocks(close, open, high, low, date, time);
+
+            console.log(close);
+            $("#database").val(data);
+
     });
+    
+});
+});
 
--- static/plot.js
+++ static/plot.js
@@ -1,1 +1,95 @@
-var chart = document.getElementById("Chart").getContext('2d');
+var chart = document.getElementById("myChart").getContext('2d');
+
+// global properties
+Chart.defaults.global.defaultFontFamily = 'Lato';
+//Chart.defaults.global.defaultFontSize = 18;
+Chart.defaults.global.defaultFontColor = '#777';
+
+const labels = Array.from(Array(10).keys()) ;
+
+function plotStocks(close, open, high, low, date, time){
+
+    // get the unique date fromt he array of dates.
+    const unique_date = [...new Set(date)];
+    
+    var stockPriceChart = new Chart(chart, {
+        type:'line',
+        data:{
+        labels: time,
+        datasets: [{
+            label:"close",
+            tension: 0,
+            data: close,//.slice(0, 10),
+            //backgroundColor:"white",
+            borderWidth: 2,
+            borderColor:'lightblue',
+            hoverBorderWidth:3,
+            hoverBorderColor:'black',
+            pointRadius: 0
+        },
+        {
+            label:"open",
+            tension: 0,
+            data: open,
+            //backgroundColor:"white",
+            borderWidth: 2,
+            borderColor:'red',
+            hoverBorderWidth:3,
+            hoverBorderColor:'red',
+            pointRadius: 0
+        },
+        {
+            label:"high",
+            tension: 0,
+            data: high,
+            //backgroundColor:"white",
+            borderWidth: 2,
+            borderColor:'green',
+            hoverBorderWidth:3,
+            hoverBorderColor:'green',
+            pointRadius: 0
+        },
+        {
+            label:"low",
+            tension: 0,
+            data: low,
+            //backgroundColor:"white",
+            borderWidth: 2,
+            borderColor:'yellow',
+            hoverBorderWidth:3,
+            hoverBorderColor:'yellow',
+            pointRadius: 0
+        }
+    ]
+        },
+        options:{
+            responsive: true,
+            maintainAspectRatio: false,
+            title:{
+                display:true,
+                text:"Stock Price of IBM, dated: " + unique_date,
+                fontSize:25
+            },
+         
+            legend:{
+                display:true,
+                position:'top',
+                labels:{
+                    fontColor:'#777'
+                }
+            },
+            layout:{
+                padding:{
+                    right:0,
+                    left:0,
+                    bottom:0,
+                    top:0
+                }
+            },
+            tooltips:{
+                enabled:true
+            }
+        }
+    });
+}
+
